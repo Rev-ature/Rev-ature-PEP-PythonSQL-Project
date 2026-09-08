71f23e7809b212723ec6d2c9cad43b077301e5e1
Rev-ature <Rev-ature@github.com>
2026-09-08 18:45:39 +0000

    Test run: 2/4 passed, 2 failed, 0 skipped - 2026-09-08T18:45:39.925Z

--- src/main/main.py
+++ src/main/main.py
@@ -67,7 +67,7 @@ def load_and_clean_users(file_path):
                     if firstName and lastName:
                         cursor.execute(
                             'INSERT INTO users(userId, firstName, lastName) VALUES (?, ?, ?)',
-                            (int(userId), firstName, lastName)
+                            (userId, firstName, lastName)
                         )
                         userId += 1
         conn.commit()
